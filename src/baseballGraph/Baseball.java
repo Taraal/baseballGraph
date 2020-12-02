9ad3c6341bdc0dade5c570b2523d430640e96cb3
Sylouan <T4raal@gmail.com>
2020-12-02 16:53:17 +0100

    Ajout .gitignore et structure du Graphe

--- src/baseballGraph/Baseball.java
+++ src/baseballGraph/Baseball.java
@@ -8,16 +8,110 @@ import java.util.Scanner;
 
 
 public class Baseball {
-	
+
+	private final class Graphe{
+
+		double CAP_MAX = 999999999;
+		ArrayList<SommetEquipe> sommetsEquipe;
+		ArrayList<SommetPaire> sommetsPaire;
+		Sommet source;
+		Sommet puits;
+
+		public class Arete{
+			public Sommet debut;
+			public Sommet fin;
+			public int valeur;
+			public int capacite;
+
+			public Arete(Sommet debut, Sommet fin, int capacite){
+				this.debut = debut;
+				this.fin = fin;
+				this.capacite = capacite;
+			}
+		}
+
+		public class Sommet{
+			public Sommet(){};
+		}
+
+
+		// Méthode de test
+		public void printNodes(){
+			for (SommetPaire i : this.sommetsPaire){
+				System.out.println("NODE :");
+				System.out.println("Equipe 1 : " + i.equipe1.name);
+				System.out.println("Equipe 2 : " + i.equipe2.name);
+			}
+		}
+
+		private final class SommetEquipe extends Sommet{
+			public Team equipe;
+
+			public SommetEquipe(Team equipe){
+				this.equipe = equipe;
+			}
+
+		}
+
+		public class SommetPaire extends Sommet{
+			public Team equipe1;
+			public Team equipe2;
+
+
+			// Besoin de comparer les sommets pour sommetsPaire.contains()
+			@Override public boolean equals(Object o){
+				if ( !(o instanceof SommetPaire)){return false;}
+				SommetPaire sp = (SommetPaire)o;
+				return (
+						(this.equipe1.name.equals(sp.equipe1.name) && this.equipe2.name.equals(sp.equipe2.name))
+						|| (this.equipe2.name.equals(sp.equipe1.name) && this.equipe1.name.equals(sp.equipe2.name))
+				);
+			}
+			// Nécessaire pour equals()
+			@Override public int hashCode(){
+				return 7 * equipe1.hashCode() + 13 * equipe2.hashCode();
+			}
+
+			public SommetPaire(Team equipe1, Team equipe2){
+				this.equipe1 = equipe1;
+				this.equipe2 = equipe2;
+			}
+
+		}
+
+		public Graphe(ArrayList<Team> equipes, Team k){
+			equipes.remove(k);
+			this.sommetsPaire = new ArrayList<SommetPaire>();
+			this.sommetsEquipe = new ArrayList<SommetEquipe>();
+
+
+			for (Team i: equipes){
+				System.out.println(i);
+				this.sommetsEquipe.add(new SommetEquipe(i));
+			}
+			for (Team i : equipes){
+				for (Team j : equipes){
+					SommetPaire sp = new SommetPaire(i, j);
+					if (i != j && !sommetsPaire.contains(sp)) {
+						this.sommetsPaire.add(new SommetPaire(i, j));
+					}
+				}
+			}
+
+			for (SommetPaire sommet : this.sommetsPaire){
+
+			}
+		}
+	}
+
 	private ArrayList<Team> teams = new ArrayList();
-	
+	Graphe graphe;
 	
 	public Baseball(String path) {
 		try {
 			File f = new File(path);
 			Scanner sc = new Scanner(f);
-	
-			
+
 			int nbEquipes = Integer.parseInt(sc.nextLine());
 			
 			
@@ -42,6 +136,9 @@ public class Baseball {
 				
 			}
 			sc.close();
+
+			this.graphe = new Graphe(this.getTeams(), this.getTeams().get(0));
+
 		} 
 		catch (FileNotFoundException e) {
 			System.out.println("An error occurred.");
@@ -61,9 +158,8 @@ public class Baseball {
 			getTeams().get(i).printTeam();
 		}
 	}
-	
 
-	private static final class Team {
+	private final class Team {
 		private int nbOfTeams;
 		
 		private int id;
@@ -96,8 +192,9 @@ public class Baseball {
 	}
 	
 	public static void main(String[] args) {
-		Baseball b = new Baseball("/home/keason/Bureau/baseballGraph/teams.txt");
-		b.printTeams();
+		Baseball b = new Baseball("/home/sylouan/Downloads/teams.txt");
+		//b.printTeams();
+		b.graphe.printNodes();
 	}
 
 }
